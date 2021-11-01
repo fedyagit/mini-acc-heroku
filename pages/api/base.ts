7ceedc8a5e7e163816b3263d06520826b516bcef
IKrystal <illia.krystal@digitalchampion.net>
2021-11-01 23:47:09 +0200

    Initial commit

---
+++ pages/api/base.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import knex from "knex";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const dbPath = path.resolve("db/testdb1.db");
+  const db = knex({
+    client: "sqlite3",
+    connection: {
+      filename: dbPath,
+    },
+    useNullAsDefault: true,
+  });
+  db.select('*').from('Items').then(userData => {
+    res.status(200).json(userData);
+  })
+  .catch(err => {
+    res.status(400).json({message: `There was an error in retrieving data`});
+  })
+
+}
