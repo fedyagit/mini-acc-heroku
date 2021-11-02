572b0385e3426203cf9d615bce09969b51ae30d4
IKrystal <illia.krystal@digitalchampion.net>
2021-11-02 23:53:39 +0200

    Updated crud

--- pages/api/base.ts
+++ pages/api/base.ts
@@ -1,21 +1,24 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import path from "path";
-import knex from "knex";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Database } from "utils";
+import { BaseApiInput, Actions, RecordInput } from "types";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const dbPath = path.resolve("db/mdb.db");
-  const db = knex({
-    client: "sqlite3",
-    connection: {
-      filename: dbPath,
-    },
-    useNullAsDefault: true,
-  });
-  db.select('*').from('Items').then(userData => {
-    res.status(200).json(userData);
-  })
-  .catch(err => {
-    res.status(400).json({message: `There was an error in retrieving data`});
-  })
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+  const { action } = req.query;
+  const { name, type, cost }: RecordInput = req.body;
+  switch (action) {
+    case Actions.Get: {
+      const result = Database.Get();
+      return result.then((data) => res.status(200).json(data));
+    }
+    case Actions.Insert: {
+      const result = Database.Insert({ name, type, cost });
+      return result.then((data) => res.status(200).json(data));
+    }
+    default:
+      return res
+        .status(200)
+        .json({ message: `${action} is not a valid action` });
+  }
+};
 
-}
+export default handler;
