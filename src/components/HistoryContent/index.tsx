b7c321b0287e6389c78285bc8a94b2f447dbe13d
Oleksandr <fedichev.oleksandr@gmail.com>
2021-11-11 19:14:49 +0200

    fix typo

--- src/components/HistoryContent/index.tsx
+++ src/components/HistoryContent/index.tsx
@@ -60,7 +60,7 @@ const HistoryContent: FC = () => {
                         scope="col"
                         className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-bold"
                       >
-                        Сумма
+                        Сума
                       </th>
                       <th
                         scope="col"
